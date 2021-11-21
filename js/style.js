6bd01d812b23e99e69929b90a334cb5cbeba3aeb
Igor <zaplatynskyy@gmail.com>
2021-11-21 16:07:25 +0100

    pulizia codice, aggiunta commenti, fine esercizio

--- js/style.js
+++ js/style.js
@@ -36,74 +36,32 @@ const team = [
     }
 ];
 
-// console.log(team);
-
-// for(let i = 0; i < team.length; i++) {
-//     // console.log(team[i]);
-//     for(const key in team[i]) {
-//         console.log(key, team[i][key]);
-//     }
-// }
-
-// const teamContainer = document.querySelector('.team-container');
-// const teamCard = document.createElement('div');
-// teamCard.className = 'team-card';
-// // console.log(teamCard); 
-// teamCard.innerHTML = `
-//     <div class="card-image">
-//         <img
-//         src="${team[0].imgSrc}"
-//         alt="${team[0].nome}"
-//         />
-//         </div>
-//     <div class="card-text">
-//     <h3>${team[0].nome}</h3>
-//     <p>${team[0].ruolo}</p>
-//     </div>
-// `;
-// console.log(teamCard.innerHTML);
-// teamContainer.appendChild(teamCard);
-
+// scorro il mio array di oggetti
 for(let i = 0; i < team.length; i++) {
+    // richiamo la funzione insTeamCard per inserire gli elementi (dati dall'array oggetti) nel document 
     insTeamCard(team[i].nome, team[i].ruolo, team[i].imgSrc);
-    // const teamContainer = document.querySelector('.team-container');
-    // const teamCard = document.createElement('div');
-    // teamCard.className = 'team-card';
-    // // console.log(teamCard); 
-    // teamCard.innerHTML = `
-    //     <div class="card-image">
-    //         <img
-    //         src="img/${team[i].imgSrc}"
-    //         alt="${team[i].nome}"
-    //         />
-    //         </div>
-    //     <div class="card-text">
-    //     <h3>${team[i].nome}</h3>
-    //     <p>${team[i].ruolo}</p>
-    //     </div>
-    // `;
-    // teamContainer.appendChild(teamCard);
 }
 
+// seleziono bottone Add al quale aggiungo un evento click
 const addMemberButton = document.getElementById('addMemberButton');
 
 addMemberButton.addEventListener("click",
     function() {
-        // console.log('funziona');
+        // prelevo i valori inseriti tramite input in delle costanti
         const nome = document.getElementById('name').value;
-        // console.log(nome);
         const ruolo = document.getElementById('role').value;
         const img = document.getElementById('image').value;
 
+        // richiamo la funzione insTeamCard per inserire gli elementi (dati in input) nel document 
         insTeamCard(nome, ruolo, img);
     }
 );
 
+// funzione per lo sviluppo ed inserimento del nuovo elemento teamCard con passaggio di argomenti quali nome ruolo e immagine del membro
 function insTeamCard(name, role, image) {
     const teamContainer = document.querySelector('.team-container');
     const teamCard = document.createElement('div');
     teamCard.className = 'team-card';
-    // console.log(teamCard); 
     teamCard.innerHTML = `
         <div class="card-image">
             <img
